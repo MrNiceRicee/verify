cc7d256a41ce25afc2df894ada07723c2bb2f280
Joshua Santos <JoshCSan@gmail.com>
2022-04-09 23:43:22 -0700

    changed roll up and changed package

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,30 @@ import typescript from 'rollup-plugin-typescript2';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'index.ts',
-  output: {
-    file: 'dist/index.es.js',
-    format: 'es',
+export default [
+  // ES Modules
+  {
+    input: 'index.ts',
+    output: {
+      file: 'dist/index.es.js',
+      format: 'es',
+    },
+    plugins: [typescript(), babel({ extensions: ['.ts'] }), terser()],
   },
-  plugins: [typescript(), babel({ extensions: ['.ts'] }), terser()],
-};
+
+  // UMD
+  {
+    input: 'index.ts',
+    output: {
+      file: 'dist/index.umd.min.js',
+      format: 'umd',
+      name: 'verifyUMD',
+      indent: false,
+    },
+    plugins: [
+      typescript(),
+      babel({ extensions: ['.ts'], exclude: 'node_modules/**' }),
+      terser(),
+    ],
+  },
+];
